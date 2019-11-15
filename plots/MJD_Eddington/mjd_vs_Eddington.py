0a0dc50ad0413e295acc07019871dfe14e4a69cd
npross <npross@lbl.gov>
2019-11-15 20:07:01 +0000

    plots/MJD_Eddington/MJD_vs_Eddington update

--- plots/MJD_Eddington/mjd_vs_Eddington.py
+++ plots/MJD_Eddington/mjd_vs_Eddington.py
@@ -15,32 +15,46 @@ from matplotlib import colors as mcolors
 from astropy.io import ascii
 from astropy.io import fits
 
-##
-##  S h e n   et al.  (2 0 1 1)   D R 7 Q  
-##
-path = '../../data/QSO_CIV_catalogs/'
-filename ='Shen_dr7_bh_May_2010.fits'
-infile = path+filename
 
-## Okay, what we really want to do ;-)
-#data_table = pyfits.getdata(infile)
-data_in    = fits.open(infile)
-data_table = data_in[1].data
+##
+##  S h e n   et al.  (2 0 1 1)
+##  D R 7 Q  
+##
+path       = '../../data/QSO_CIV_catalogs/'
+filename   = 'Shen_dr7_bh_May_2010.fits'
+dr7q_in    = fits.open(path+filename)
+dr7q_table = dr7q_in[1].data
 
 ## Quick check on the format/dimensions of the FITS table file...
-print(type(data_table), '\n')
-print('The number of rows of is.... ',     data_table.shape,  '\n')
-print('The number of columns is...  ', len(data_table.names), '\n\n')
-
+print(type(dr7q_table), '\n')
+print('The number of rows of is.... ',     dr7q_table.shape,  '\n')
+print('The number of columns is...  ', len(dr7q_table.names), '\n\n')
 
 ## Making some senible selections and cuts 
-mjd_full    = data_table.field('MJD')
-logtau_full = data_table.field('LOGEDD_RATIO')
+mjd_full    = dr7q_table.field('MJD')
+logtau_full = dr7q_table.field('LOGEDD_RATIO')
 
-dr7q        = data_table[np.where(data_table['LOGEDD_RATIO'] > -9.0)]
+dr7q        = dr7q_table[np.where(dr7q_table['LOGEDD_RATIO'] > -9.0)]
 dr7q_mjd    = dr7q['MJD']
 dr7q_logtau = dr7q['LOGEDD_RATIO']
 dr7q_tau    = (10**dr7q['LOGEDD_RATIO'])*100
+
+
+## Kozlowski (2017)  and  Hamann et al. (2017)
+##  D R 1 2 Q 
+filename   = 'BOSS_Ham17Koz17_DR12Q.fits'
+dr12q_in    = fits.open(path+filename)
+dr12q_table = dr12q_in[1].data
+
+## Quick check on the format/dimensions of the FITS table file...
+print(type(dr12q_table), '\n')
+print('The number of rows of is.... ',     dr12q_table.shape,  '\n')
+print('The number of columns is...  ', len(dr12q_table.names), '\n\n')
+
+## Making some senible selections and cuts 
+dr12q        = dr12q_table[np.where(dr12q_table['nEdd'] > -9.0)]
+dr12q_mjd    = dr12q['MJD_x']
+dr12q_etaEdd = dr12q['nEdd']
 
     
 ## The data for our CIV CLQs
@@ -53,7 +67,7 @@ CLQs   = ascii.read(path+infile)
 fig, ax = plt.subplots(figsize=(10.0, 6.0), dpi=80, facecolor='w', edgecolor='k')   # was 14.0, 8.0
 
 ## Adjusting the Whitespace for the plots
-left   = 0.08   # the left side of the subplots of the figure
+left   = 0.10   # the left side of the subplots of the figure
 right  = 0.96   # the right side of the subplots of the figure
 bottom = 0.12   # the bottom of the subplots of the figure
 top    = 0.96   # the top of the subplots of the figure
@@ -86,8 +100,8 @@ gridsize  = 30  #use log bins
 mincnt    = .1
 
 ##  T H E     D R 7 Q     q u a s a r s 
-ax.hexbin(dr7q_mjd, dr7q_logtau,         bins='log',  gridsize=gridsize, cmap=color_map, mincnt=mincnt)
-
+ax.hexbin( dr7q_mjd,  dr7q_logtau, bins='log',  gridsize=gridsize, cmap=color_map, mincnt=mincnt)
+ax.hexbin(dr12q_mjd, dr12q_etaEdd, bins='log',  gridsize=gridsize, cmap=color_map, mincnt=mincnt)
 
 
 
@@ -148,31 +162,19 @@ for ii in range(len(CLQs)):
             M87_leg  = mlines.Line2D([], [], label='M87 (57854)', color='darkorange',
                         marker="H", markeredgecolor='k', markeredgewidth=1.4, markersize=7,  linestyle='None')
 
-#ax.scatter(CLQs['MJD'], CLQs['eta_MgII'], marker='o', s=ms_large*1.6, color='black')
-#ax.scatter(CLQs['MJD'], CLQs['eta_MgII'], marker='o', s=ms_large,     color='dodgerblue')
-
-#ax.scatter(risers_data['mjd'], np.log10(risers_data['EDD_RATIO']/100),  marker='o', s=ms_large*1.6, color='black')
-#ax.scatter(risers_data['mjd'], np.log10(risers_data['EDD_RATIO']/100),  marker='o', s=ms_large, color='red')
-
-
-
 ## Tidy up the figure
 xmin     = 51400      ## 51100
 xmax     = 58664.+360
-ymin     =   -5.1     ## -3.4 
+ymin     =   -2.8     ##  used to be -5.1
 ymax     =   1.4      ##  1.2
 ymin_log =   0.1      ##  0.1
 ymax_log = 100.0    
 
 ax.set_xlim((xmin, xmax))
 ax.set_ylim((ymin, ymax))
-#ax.set_yscale('log')
-#ax.set_ylim((ymin_log, ymax_log))
 
 ax.tick_params('x', direction='in')
 ax.tick_params('y', direction='in')
-#ax.minorticks_on('x', direction='in')
-#ay.minorticks_on()
 ax.tick_params('x', direction='in', which='major', bottom='True', top='True', left='True', right='True', labelsize=fontsize/1.2)
 ax.tick_params('x', direction='in', which='minor', bottom='True', top='True', left='True', right='True', labelsize=fontsize/1.2)
 ax.tick_params('y', direction='in', which='both',  bottom='True', top='True', left='True', right='True', labelsize=fontsize)
@@ -186,25 +188,26 @@ log_NodaDone_max = np.log10(NodaDone * NodaDone_range)
 
 ax.axhspan(log_NodaDone_min, log_NodaDone_max, alpha=0.6, color='red')
 ax.text( log_NodaDone_min,   0.8, 'Noda-Done',   style='italic',    fontsize=fontsize/1.2, rotation=270)
+ax.text( 51800,   -1.8, 'Noda-Done',   style='italic', weight='bold',   fontsize=fontsize/1.1)
 
 ax.set_xlabel('MJD',                       fontsize=fontsize)
 ax.set_ylabel(r'log$_{10}$ Eddington Ratio', fontsize=fontsize)
 
 
-
 ##
 ##  L E G E N D S
 ##
-boss  = mlines.Line2D([], [], label='DR7 quasars', color='grey',
+boss  = mlines.Line2D([], [], label='SDSS/BOSS quasars', color='grey',
                           marker=".", markeredgecolor='k', markeredgewidth=1.2,
                           markersize=8,  linestyle='None')
 
 handles=[boss, J12_53498, J12_58693,
                J16_54553, J16_55832, J16_58583, 
-               J22_56189, J22_56960, J22_58693, M87_leg]
-leg = ax.legend(loc='lower left',
-                fontsize=fontsize/1.4, handles=handles, 
-                frameon=True, framealpha=1.0, fancybox=True, ncol=2)
+               J22_56189, J22_56960, J22_58693]
+#               M87_leg]
+leg = ax.legend(loc='upper right',
+                fontsize=fontsize/1.6, handles=handles, 
+                frameon=True, framealpha=1.0, fancybox=True, ncol=3)
 
 
 
